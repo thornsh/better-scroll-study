15c837ca4a3f346f4f900b77f2978d7234ec0415
兰世豪 <1183643226@qq.com>
2021-01-19 11:41:12 +0800

    双重滚动的基本实现

--- src/index.js
+++ src/index.js
@@ -15,17 +15,34 @@ let extraTransform = {
 }
 // bsInside.scrollTo(0, -60, 300, undefined, extraTransform)
 
-let bsOutsideContainer = BetterScroll.createBScroll('.wrapper-inside-container',{
-  specifiedIndexAsContent:1,
-  bindToTarget: true,
-  scrollX: true,
-  scrollY: true,
-  freeScroll: true,
-  bounce: true,
-  movable: true, // for movable plugin
-  tagException:{className:/(^|\s)wrapper-outside(\s|$)/}
-})
+//上下移动
+// let bsOutsideContainer = BetterScroll.createBScroll('.wrapper-inside-container',{
+//   specifiedIndexAsContent:1,
+//   bindToTarget: true,
+//   scrollX: true,
+//   scrollY: true,
+//   freeScroll: true,
+//   bounce: true,
+//   movable: true, // for movable plugin
+//   tagException:{className:/(^|\s)wrapper-outside(\s|$)/}
+// })
+//文字移动
 let bsOutside = BetterScroll.createBScroll('.wrapper-outside-container',{
-  specifiedIndexAsContent:1,
-  tagException:{className:/(^|\s)wrapper-outside-container(\s|$)/}
+  
 })
+
+
+let wrapperTop = BetterScroll.createBScroll('.wrapper-outside-div',{})
+const hooks = wrapperTop.scroller.actionsHandler.hooks
+  hooks.on('move',function(e){
+    console.log(e.e.path[0].getAttribute("data-move"));
+    let dataMove = e.e.path[0].getAttribute("data-move");
+    if(dataMove!=null){
+      console.log(e.deltaY);
+      $(".wrapper-outside-container").css("height",$(".wrapper-outside-container").height()-e.deltaY);
+      $(".wrapper-inside").css("height",$(".wrapper-inside").height()+e.deltaY);
+      bsOutside.refresh();
+      bsInside.refresh();
+    }
+    
+  })
